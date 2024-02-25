8d549a53adcece612c3f3b8c93a964e5fc596ef7
yauhenii <yauhenmardan@gmail.com>
2024-02-25 14:35:25 +0100

    add input for dataset

--- core/dataset/DatasetHandler.py
+++ core/dataset/DatasetHandler.py
@@ -1,6 +1,7 @@
 from core.dataset.loader import AbstractLoader, DatasetLoaderFactory
 from core.dataset.split_strategy import (AbstractSplitStrategy,
                                          SplitStrategyFactory)
+from typing import Dict
 
 
 class DatasetHandler:
@@ -8,18 +9,15 @@ class DatasetHandler:
     Class to handle datasets and split strategies.
     """
 
-    def __init__(self, dataset_name: str, dataset_path: str, split_strategy_name: str):
+    def __init__(self, dataset_config: Dict, split_strategy_config: Dict):
         """
         Initialize DatasetHandler with dataset and split strategy information.
 
         Args:
-            dataset_name (str): Name of the dataset.
-            dataset_path (str): Path to the dataset.
-            split_strategy_name (str): Name of the split strategy.
+            # TODO: change
         """
-        self._dataset_name = dataset_name
-        self._dataset_path = dataset_path
-        self._split_strategy_name = split_strategy_name
+        self._dataset_config = dataset_config
+        self._split_strategy_config = split_strategy_config
         self._dataset_loader = None
         self._split_strategy = None
 
@@ -42,13 +40,13 @@ class DatasetHandler:
         Load dataset and split it using the specified strategy.
         """
         dataset_loader = DatasetLoaderFactory().create(
-            dataset_name=self._dataset_name, dataset_path=self._dataset_path
+            dataset_name=self._dataset_config['dataset_name'], dataset_path=self._dataset_config['dataset_path']
         )
         split_strategy = SplitStrategyFactory().create(
-            split_strategy_name=self._split_strategy_name
+            split_strategy_name=self._split_strategy_config['split_strategy_name']
         )
 
-        split_strategy.split(dataset_loader=dataset_loader)
+        split_strategy.split(dataset_loader=dataset_loader, split_config=self._split_strategy_config)
 
         self._dataset_loader = dataset_loader
         self._split_strategy = split_strategy
--- scripts/run.py
+++ scripts/run.py
@@ -7,11 +7,9 @@ from core.dataset import DatasetHandler
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def main(config: DictConfig) -> None:
     print(config)
-    dataset_handler = DatasetHandler(dataset_name=config["experiment_settings"]["dataset_name"],
-                                     dataset_path=config["experiment_settings"]["dataset_path"],
-                                     split_strategy_name=config["experiment_settings"]["split_strategy_name"])
+    dataset_handler = DatasetHandler(dataset_config=config["experiment_settings"]["dataset"],
+                                     split_strategy_config=config["experiment_settings"]["split_strategy"])
     dataset_handler.load_and_split_dataset()
-
 
 
 if __name__ == '__main__':
