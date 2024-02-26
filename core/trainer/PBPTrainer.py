d0abe269f3cf34a909f1d80fa9656d7c54c703ac
yauhenii <yauhenmardan@gmail.com>
2024-02-26 15:19:23 +0100

    add docstring to KLDivergenceInterface

--- core/trainer/PBPTrainer.py
+++ core/trainer/PBPTrainer.py
@@ -6,4 +6,8 @@ from core.trainer import AbstractTrainer
 
 class PBPTrainer(AbstractTrainer):
     def train(self, model: AbstractModel, training_config: Dict) -> AbstractModel:
-        pass
+        epochs = training_config['epochs']
+        disable_tqdm = training_config['disable_tqdm']
+
+        for epoch in trange(epochs, disable=disable_tqdm):
+
--- core/utils/KLDivergenceInterface.py
+++ core/utils/KLDivergenceInterface.py
@@ -4,6 +4,19 @@ import torch
 
 
 class KLDivergenceInterface(ABC):
+    """
+    An abstract base class for computing Kullback-Leibler Divergence (KL Divergence).
+    """
     @abstractmethod
     def compute_kl(self, *args, **kwargs) -> torch.Tensor:
+        """
+        Computes the Kullback-Leibler Divergence (KL Divergence) between two probability distributions.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            torch.Tensor: The computed KL Divergence.
+        """
         pass
