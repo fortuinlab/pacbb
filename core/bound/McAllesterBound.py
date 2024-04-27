fa15fdc593fe02cc47f088d455daf3651a88772b
yauhenii <yauhenmardan@gmail.com>
2024-04-27 15:42:26 +0200

    add mc allister bound; restructure inv kl usage and bound calculation

--- core/bound/McAllesterBound.py
+++ core/bound/McAllesterBound.py
@@ -7,9 +7,9 @@ from core.bound import AbstractBound
 from core.utils.kl import inv_kl
 
 
-class KLBound(AbstractBound):
+class McAllesterBound(AbstractBound):
     """
-        Implements a PAC Bayes KL bound.
+        Implements a McAllester PAC Bayes bound.
     """
 
     def __init__(self, bound_delta: float, loss_delta: float):
@@ -35,5 +35,5 @@ class KLBound(AbstractBound):
                 A tuple containing the calculated PAC Bayes bound and the upper bound of empirical risk.
         """
         empirical_risk = inv_kl(avg_loss, math.log(2 / self._loss_delta) / num_samples_loss)
-        risk = inv_kl(empirical_risk, (kl + math.log((2 * math.sqrt(num_samples_bound)) / self._bound_delta)) / num_samples_bound)
+        risk = empirical_risk + math.sqrt((kl + math.log((2 * math.sqrt(num_samples_bound))) / self._bound_delta) / (2 * num_samples_bound))
         return risk, empirical_risk
--- core/bound/__init__.py
+++ core/bound/__init__.py
@@ -1,2 +1,4 @@
 from core.bound.AbstractBound import AbstractBound
 from core.bound.KLBound import KLBound
+from core.bound.McAllesterBound import McAllesterBound
+
