806d3747e438355a7aca1fa01e55874a4e85e9af
yauhenii <yauhenmardan@gmail.com>
2024-09-28 10:18:58 +0200

    ignore .ipynb_checkpoints

--- jobs/runnig_configs_in_the_folder.sh
+++ jobs/runnig_configs_in_the_folder.sh
@@ -12,8 +12,8 @@ if [ -z "$CONFIG_DIR" ]; then
   exit 1
 fi
 
-# Find all YAML files in the given directory and its subdirectories
-find "$PROJECT_ROOT/$CONFIG_DIR" -type f -name "*.yaml" | while read config_file; do
+# Find all YAML files in the given directory and its subdirectories, ignoring .ipynb_checkpoints
+find "$PROJECT_ROOT/$CONFIG_DIR" -type f -name "*.yaml" -not -path "*/.ipynb_checkpoints/*" | while read config_file; do
   echo "Running experiment with config: $config_file"
 
   # Run the Python script from the project root with the found YAML config
