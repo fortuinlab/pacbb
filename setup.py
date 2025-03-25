fcc241e31b0bd0895c5fbd8f1f5a5d4df7121363
Yauhenii <34161697+Yauhenii@users.noreply.github.com>
2025-03-25 19:42:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='pacbb',
-    version='0.1',
+    version='0.1.1',
     description='PAC Bayes Bound toolset',
     long_description=long_description,
     long_description_content_type='text/markdown',
